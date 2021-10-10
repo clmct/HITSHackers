00eef1c37ab036d8de0f498060eee3f78bf3e639
Daniil <krivelevdaniil@gmail.com>
2021-10-10 14:18:49 +0700

    фвыфыв

--- InvestmentGame/Modules/ScreenLearn/ViewControllerLearn.swift
+++ InvestmentGame/Modules/ScreenLearn/ViewControllerLearn.swift
@@ -7,6 +7,31 @@
 
 import UIKit
 
+class ViewControllerLearn2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func clicks(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
 class ViewControllerLearn: UIViewController {
 
     override func viewDidLoad() {
